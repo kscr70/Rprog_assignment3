a3d1983c606e3edf0ffef2cafd0205815f5c2840
Scott Riley <kscr70@gmail.com>
2015-01-26 19:46:26 -0800

    true working version.

--- best.R
+++ best.R
@@ -5,40 +5,35 @@ best <- function(state, outcome){
         ## Check that state and outcome are valid
         if ((!is.element (state, outcome_data [, 7])) == TRUE) {  #checks whether the state entered is in the state column
                 stop("invalid state")
-        }else{
-                print(state)
         }
         
         if ((!is.element (outcome, valid_outcome)) == TRUE) {    #checks whether outcome entered is valid
                 stop("invalid outcome")
-        } else {
-                print(outcome)
-        }
+        } 
         ## Return hospital name in that state with lowest 30-day death rate
-        state_split <<- split(outcome_data, outcome_data$State)
-        
-<<<<<<< HEAD
-}
-=======
-        By_state <<- state_split[[state]]                
+        state_split <- split(outcome_data, outcome_data$State)
+       
+        By_state <- state_split[[state]]                
         
         if (outcome == "heart attack") {
-                out <- By_state$Hospital.Name[which.min(By_state[,11])]
+                out <- as.character(By_state$Hospital.Name[which.min(By_state[,11])])
                 
                 sort_out <- sort(out)
-                print(sort_out[1])
+                sort_out_final <- (sort_out[1])
+                
+                return(sort_out_final)
         }
         if (outcome == "heart failure"){
-                out2 <- By_state$Hospital.Name[which.min(By_state[ ,17])]
+                out2 <- as.character(By_state$Hospital.Name[which.min(By_state[ ,17])])
                 
                 sort_out2 <- sort(out2)
-                print(sort_out2[1])
+                return(sort_out2[1])
         }
         if (outcome == "pneumonia"){
-                out3 <- By_state$Hospital.Name[which.min(By_state[ ,23])]
+                out3 <- as.character(By_state$Hospital.Name[which.min(By_state[ ,23])])
                 sort_out3 <- sort(out3)
-                print(sort_out3[1])
+                return(sort_out3[1])
                 
         }
 }
->>>>>>> 8f2f7912d515e3174fecf69b594e3631e0d1ccdb
+
